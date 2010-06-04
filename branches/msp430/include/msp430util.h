bcdedc5c33430cee992bd032c728746c5de68ea0
conradoplg <conradoplg@2740c9e8-7662-11de-98ce-ab17797ee0eb>
2010-06-04 19:54:04 +0000

    Fixed msp430util.h

--- branches/msp430/include/msp430util.h
+++ branches/msp430/include/msp430util.h
@@ -3,12 +3,12 @@
 
 unsigned long long msp430_get_cycles();
 
-volatile unsigned char TEST_CMD asm("0x01b0");
-volatile unsigned char TEST_TEXTOUT asm("0x01b1");
-volatile unsigned int BENCH_CYCLES_0 asm("0x01b2");
-volatile unsigned int BENCH_CYCLES_1 asm("0x01b3");
-volatile unsigned int BENCH_CYCLES_2 asm("0x01b4");
-volatile unsigned int BENCH_CYCLES_3 asm("0x01b5");
+volatile unsigned char TEST_CMD __asm__("0x01b0");
+volatile unsigned char TEST_TEXTOUT __asm__("0x01b1");
+volatile unsigned int BENCH_CYCLES_0 __asm__("0x01b2");
+volatile unsigned int BENCH_CYCLES_1 __asm__("0x01b3");
+volatile unsigned int BENCH_CYCLES_2 __asm__("0x01b4");
+volatile unsigned int BENCH_CYCLES_3 __asm__("0x01b5");
 
 #define BENCH_RESET_             0x50    //Reset cycle counter
 #define BENCH_START_             0x51    //Start counting cycles
