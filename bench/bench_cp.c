f1a947a5faafb4b429d2e5e7dadbc6f22c12c255
Diego F. Aranha <dfaranha@gmail.com>
2015-01-26 23:56:16 -0200

    Added benchmarks for BF-IBE protocol.

--- bench/bench_cp.c
+++ bench/bench_cp.c
@@ -344,8 +344,6 @@ static void ecies(void) {
 	ec_new(r);
 	bn_new(d);
 
-	rand_bytes(in, sizeof(in));
-
 	BENCH_BEGIN("cp_ecies_gen") {
 		BENCH_ADD(cp_ecies_gen(d, q));
 	}
@@ -474,8 +472,6 @@ static void sokaka(void) {
 	sokaka_new(k);
 	bn_new(s);
 
-	cp_sokaka_gen(s);
-
 	BENCH_BEGIN("cp_sokaka_gen") {
 		BENCH_ADD(cp_sokaka_gen(s));
 	}
@@ -504,6 +500,57 @@ static void sokaka(void) {
 
 	sokaka_free(k);
 	bn_free(s);
+}
+
+static void ibe(void) {
+	bn_t s;
+	g1_t pub;
+	g2_t prv;
+	uint8_t in[10], out[10 + 2 * FP_BYTES + 1];
+	char id[5] = { 'A', 'l', 'i', 'c', 'e' };
+	int in_len, out_len;
+
+	bn_null(s);
+	g1_null(pub);
+	g2_null(prv);
+
+	bn_new(s);
+	g1_new(pub);
+	g2_new(prv);
+
+	rand_bytes(in, sizeof(in));
+
+	BENCH_BEGIN("cp_ibe_gen") {
+		BENCH_ADD(cp_ibe_gen(s, pub));
+	}
+	BENCH_END;
+
+	BENCH_BEGIN("cp_ibe_gen_prv") {
+		BENCH_ADD(cp_ibe_gen_prv(prv, id, sizeof(id), s));
+	}
+	BENCH_END;
+
+	BENCH_BEGIN("cp_ibe_enc") {
+		in_len = sizeof(in);
+		out_len = in_len + 2 * FP_BYTES + 1;
+		rand_bytes(in, sizeof(in));
+		BENCH_ADD(cp_ibe_enc(out, &out_len, in, in_len, id, sizeof(id), pub));
+		cp_ibe_dec(out, &out_len, out, out_len, prv);
+	}
+	BENCH_END;
+
+	BENCH_BEGIN("cp_ecies_dec") {
+		in_len = sizeof(in);
+		out_len = in_len + 2 * FP_BYTES + 1;
+		rand_bytes(in, sizeof(in));
+		cp_ibe_enc(out, &out_len, in, in_len, id, sizeof(id), pub);
+		BENCH_ADD(cp_ibe_dec(out, &out_len, out, out_len, prv));
+	}
+	BENCH_END;
+
+	ec_free(q);
+	ec_free(r);
+	bn_free(d);
 }
 
 static void bls(void) {
@@ -625,6 +672,7 @@ int main(void) {
 	util_banner("Protocols based on pairings:\n", 0);
 	if (pc_param_set_any() == STS_OK) {
 		sokaka();
+		ibe();
 		bls();
 		bbs();
 	} else {
