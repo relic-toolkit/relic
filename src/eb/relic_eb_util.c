73e9f2a94732a26775a207feb286a4c73bafa14f
Diego F. Aranha <dfaranha@gmail.com>
2015-02-24 23:59:48 -0300

    New formulas for precomputation in Koblitz curves.

--- src/eb/relic_eb_util.c
+++ src/eb/relic_eb_util.c
@@ -213,432 +213,253 @@ void eb_tab(eb_t *t, const eb_t p, int w) {
 			t[i]->norm = 1;
 		}
 
-#if defined(EB_MIXED)
-		eb_norm(t[0], p);
-#else
-		eb_copy(t[0], p);
-#endif
-
 		switch (w) {
+			/* Formulas from https://eprint.iacr.org/2014/664. */
 #if EB_DEPTH == 3 || EB_WIDTH ==  3
 			case 3:
-				eb_frb(t[1], t[0]);
+				eb_frb(t[0], p);
 				if (u == 1) {
-					eb_sub(t[1], t[0], t[1]);
-				} else {
-					eb_add(t[1], t[0], t[1]);
+					eb_neg(t[0], t[0]);
 				}
+				eb_add(t[1], t[0], p);
+				eb_copy(t[0], p);
 				break;
 #endif
 #if EB_DEPTH == 4 || EB_WIDTH ==  4
 			case 4:
-				eb_frb(t[3], t[0]);
-				eb_frb(t[3], t[3]);
-
-				eb_sub(t[1], t[3], p);
-				eb_add(t[2], t[3], p);
-				eb_frb(t[3], t[3]);
-
-				if (u == 1) {
-					eb_neg(t[3], t[3]);
+				eb_frb(t[0], p);
+				eb_frb(t[1], t[0]);
+				if (u == -1) {
+					eb_neg(t[0], t[0]);
 				}
-				eb_sub(t[3], t[3], p);
+				eb_sub(t[2], t[0], p);
+				eb_add(t[3], t[0], p);
+				eb_sub(t[1], t[1], p);
+				eb_copy(t[0], p);
 				break;
 #endif
 #if EB_DEPTH == 5 || EB_WIDTH ==  5
 			case 5:
-				eb_frb(t[3], t[0]);
-				eb_frb(t[3], t[3]);
-
-				eb_sub(t[1], t[3], p);
-				eb_add(t[2], t[3], p);
-				eb_frb(t[3], t[3]);
-
-				eb_frb(t[7], t[3]);
-				eb_sub(t[7], t[7], p);
-
-				if (u == 1) {
-					eb_neg(t[3], t[3]);
-				}
-				eb_sub(t[3], t[3], p);
-
-				eb_frb(t[4], t[2]);
-				eb_frb(t[4], t[4]);
-
-				eb_neg(t[4], t[4]);
-				eb_sub(t[5], t[4], p);
-				eb_add(t[6], t[4], p);
-
-				eb_frb(t[4], t[4]);
+				eb_frb(t[0], p);
+				eb_frb(t[1], t[0]);
 				if (u == -1) {
-					eb_neg(t[4], t[4]);
+					eb_neg(t[0], t[0]);
 				}
-				eb_add(t[4], t[4], p);
+				eb_sub(t[2], t[0], p);
+				eb_add(t[3], t[0], p);
+				eb_sub(t[1], t[1], p);
+				eb_add(t[4], t[1], t[0]);
+				eb_add(t[5], t[2], t[0]);
+				eb_add(t[6], t[3], t[0]);
+				eb_neg(t[7], t[5]);
+				eb_sub(t[7], t[7], t[0]);
+				eb_copy(t[0], p);
 				break;
 #endif
 #if EB_DEPTH == 6 || EB_WIDTH ==  6
 			case 6:
-				eb_frb(t[0], t[0]);
-				eb_frb(t[0], t[0]);
-				eb_neg(t[14], t[0]);
-
-				eb_sub(t[13], t[14], p);
-				eb_add(t[14], t[14], p);
-
-				eb_frb(t[0], t[0]);
-				eb_frb(t[8], t[0]);
-				if (u == -1) {
+				eb_frb(t[0], p);
+				eb_frb(t[15], t[0]);
+				eb_neg(t[15], t[15]);
+				if (u == 1) {
 					eb_neg(t[0], t[0]);
 				}
-				eb_sub(t[11], t[0], p);
-				eb_add(t[12], t[0], p);
-
-				eb_frb(t[0], t[12]);
-				eb_frb(t[0], t[0]);
-				eb_sub(t[1], t[0], p);
-				eb_add(t[2], t[0], p);
-
-				eb_frb(t[15], t[8]);
-				if (u == -1) {
-					eb_neg(t[15], t[15]);
-				}
-				eb_sub(t[15], t[15], p);
-
-				eb_frb(t[0], t[13]);
-				eb_frb(t[0], t[0]);
-				eb_sub(t[5], t[0], p);
-				eb_add(t[6], t[0], p);
-
-				eb_sub(t[7], t[8], p);
-				eb_add(t[8], t[8], p);
-
-				eb_frb(t[0], t[0]);
-				if (u == -1) {
-					eb_neg(t[0], t[0]);
-				}
-				eb_sub(t[3], t[0], p);
-				eb_add(t[4], t[0], p);
-
-				eb_frb(t[0], t[1]);
-				eb_frb(t[0], t[0]);
-
-				eb_neg(t[9], t[0]);
-				eb_sub(t[9], t[9], p);
-
-				eb_frb(t[0], t[14]);
-				eb_frb(t[0], t[0]);
-				eb_add(t[10], t[0], p);
-
+				eb_sub(t[12], t[0], p);
+				eb_add(t[13], t[0], p);
+				eb_neg(t[6], t[12]);
+				eb_sub(t[6], t[6], t[0]);
+				eb_neg(t[5], t[13]);
+				eb_sub(t[5], t[5], t[0]);
+				eb_neg(t[7], t[5]);
+				eb_add(t[7], t[7], t[0]);
+				eb_add(t[14], t[15], p);
+				eb_sub(t[1], t[14], t[0]);
+				eb_neg(t[4], t[14]);
+				eb_sub(t[4], t[4], t[0]);
+				eb_neg(t[11], t[1]);
+				eb_add(t[11], t[11], t[0]);
+				eb_neg(t[8], t[4]);
+				eb_add(t[8], t[8], t[0]);
+				eb_neg(t[10], t[8]);
+				eb_sub(t[10], t[10],t[0]);
+				eb_add(t[15], t[1], t[15]);
+				eb_sub(t[2], t[15], t[0]);
+				eb_neg(t[3], t[15]);
+				eb_sub(t[3], t[3], t[0]);
+				eb_neg(t[9], t[3]);
+				eb_add(t[9], t[9], t[0]);
 				eb_copy(t[0], p);
 				break;
 #endif
 #if EB_DEPTH == 7 || EB_WIDTH ==  7
-			/**
-			 * Formulas from http://eprint.iacr.org/2012/519
-			 */
 			case 7:
-				eb_frb(t[0], t[0]);
-				eb_frb(t[0], t[0]);
-				eb_sub(t[17], t[0], p);
-				eb_add(t[18], t[0], p);
-				eb_frb(t[0], t[0]);
-				if (u == 1) {
-					eb_neg(t[0], t[0]);
-				}
-				eb_sub(t[19], t[0], p);
-				eb_add(t[20], t[0], p);
-
-				eb_frb(t[0], t[19]);
-				eb_frb(t[0], t[0]);
-				eb_frb(t[11], t[0]);
-				if (u == 1) {
-					eb_neg(t[11], t[11]);
-				}
-				eb_add(t[12], t[11], p);
-				eb_sub(t[11], t[11], p);
-				eb_neg(t[0], t[0]);
-				eb_sub(t[1], t[0], p);
-				eb_add(t[2], t[0], p);
-
-				eb_frb(t[0], t[17]);
-				eb_frb(t[0], t[0]);
-				eb_frb(t[0], t[0]);
+				eb_frb(t[0], p);
+				eb_frb(t[15], t[0]);
 				if (u == -1) {
 					eb_neg(t[0], t[0]);
 				}
-				eb_sub(t[3], t[0], p);
+				eb_sub(t[18], t[0], p);
+				eb_add(t[19], t[0], p);
+				eb_neg(t[26], t[18]);
+				eb_sub(t[26], t[26], t[0]);
+				eb_neg(t[25], t[19]);
+				eb_sub(t[25], t[25], t[0]);
+				eb_sub(t[7], t[26], t[0]);
+				eb_sub(t[6], t[25], t[0]);
+				eb_neg(t[11], t[7]);
+				eb_add(t[11], t[11], t[0]);
+				eb_neg(t[12], t[6]);
+				eb_add(t[12], t[12], t[0]);
+				eb_add(t[30], t[11], t[0]);
+				eb_add(t[31], t[12], t[0]);
+				eb_neg(t[14], t[30]);
+				eb_sub(t[14], t[14], t[0]);
 
-				eb_frb(t[0], t[1]);
-				eb_frb(t[0], t[0]);
-				eb_add(t[9], t[0], p);
-				eb_neg(t[9], t[9]);
-				eb_frb(t[0], t[0]);
-				if (u == -1) {
-					eb_neg(t[0], t[0]);
-				}
-				eb_add(t[4], t[0], p);
-
-				eb_frb(t[0], t[18]);
-				eb_frb(t[0], t[0]);
-				eb_sub(t[7], t[0], t[18]);
-				eb_sub(t[25], t[0], p);
-				eb_add(t[26], t[0], p);
-				eb_frb(t[0], t[0]);
-				if (u == -1) {
-					eb_neg(t[0], t[0]);
-				}
-				eb_sub(t[27], t[0], p);
-				eb_add(t[28], t[0], p);
-
-				eb_frb(t[0], t[17]);
-				eb_frb(t[0], t[0]);
-				eb_add(t[8], t[0], t[18]);
-				eb_neg(t[0], t[0]);
-				eb_add(t[10], t[0], p);
-
-				eb_frb(t[0], t[7]);
-				eb_frb(t[0], t[0]);
-				eb_sub(t[13], t[0], p);
-				eb_frb(t[0], t[8]);
-				eb_frb(t[0], t[0]);
-				eb_neg(t[0], t[0]);
-				eb_add(t[14], t[0], p);
-
-				eb_frb(t[0], t[19]);
-				eb_frb(t[0], t[0]);
-				eb_add(t[15], t[0], t[17]);
-				eb_add(t[16], t[0], t[18]);
-
-				eb_frb(t[0], t[26]);
-				eb_frb(t[0], t[0]);
-				eb_neg(t[0], t[0]);
-				eb_sub(t[5], t[0], p);
-				eb_add(t[6], t[0], p);
-				eb_add(t[23], t[0], t[17]);
-				eb_add(t[24], t[0], t[18]);
-
-				eb_frb(t[0], t[25]);
-				eb_frb(t[0], t[0]);
-				eb_sub(t[21], t[0], p);
-				eb_add(t[22], t[0], p);
-
-				eb_frb(t[0], t[20]);
-				eb_frb(t[0], t[0]);
-				eb_add(t[29], t[0], p);
-				eb_neg(t[29], t[29]);
-
-				eb_frb(t[0], t[27]);
-				eb_frb(t[0], t[0]);
-				eb_add(t[30], t[0], p);
-
-				eb_frb(t[0], t[7]);
-				eb_frb(t[0], t[0]);
-				eb_add(t[31], t[0], t[17])
-
-						eb_copy(t[0], p);
+				eb_sub(t[17], t[15], p);
+				eb_neg(t[1], t[17]);
+				eb_sub(t[27], t[1], t[0]);
+				eb_add(t[1], t[1], t[0]);
+				eb_add(t[20], t[1], t[0]);
+				eb_sub(t[8], t[27], t[0]);
+				eb_neg(t[24], t[20]);
+				eb_sub(t[24], t[24], t[0]);
+				eb_neg(t[10], t[8]);
+				eb_add(t[10], t[10], t[0]);
+				eb_sub(t[5], t[24], t[0]);
+				eb_neg(t[13], t[5]);
+				eb_add(t[13], t[13], t[0]);
+				eb_neg(t[16], t[1]);
+				eb_add(t[16], t[16], t[15]);
+				eb_neg(t[2], t[16]);
+				eb_sub(t[28], t[2], t[0]);
+				eb_add(t[2], t[2], t[0]);
+				eb_add(t[21], t[2], t[0]);
+				eb_sub(t[9], t[28], t[0]);
+				eb_neg(t[23], t[21]);
+				eb_sub(t[23], t[23], t[0]);
+				eb_sub(t[4], t[23], t[0]);
+				eb_neg(t[3], t[2]);
+				eb_add(t[15], t[3], t[15]);
+				eb_neg(t[3], t[15]);
+				eb_add(t[3], t[3], t[0]);
+				eb_neg(t[29], t[15]);
+				eb_sub(t[29], t[29], t[0]);
+				eb_add(t[22], t[3], t[0]);
+				eb_copy(t[0], p);
 				break;
 #endif
 #if EB_DEPTH == 8 || EB_WIDTH ==  8
-			/**
-			 * Formulas from http://eprint.iacr.org/2012/519
-			 */
 			case 8:
-				eb_frb(t[0], t[0]);
-				eb_frb(t[0], t[0]);
-				eb_neg(t[0], t[0]);
-				eb_sub(t[45], t[0], p);
-				eb_add(t[46], t[0], p);
-				eb_frb(t[0], t[0]);
+				eb_frb(t[0], p);
+				eb_frb(t[57], t[0]);
+				eb_neg(t[57], t[57]);
 				if (u == 1) {
 					eb_neg(t[0], t[0]);
 				}
-				eb_sub(t[43], t[0], p);
-				eb_add(t[44], t[0], p);
+				eb_sub(t[44], t[0], p);
+				eb_add(t[45], t[0], p);
+				eb_neg(t[38], t[44]);
+				eb_sub(t[38], t[38], t[0]);
+				eb_neg(t[37], t[45]);
+				eb_sub(t[37], t[37], t[0]);
+				eb_neg(t[6], t[38]);
+				eb_add(t[6], t[6], t[0]);
 
-				eb_frb(t[0], t[44]);
-				eb_frb(t[0], t[0]);
-				eb_sub(t[1], t[0], p);
-				eb_add(t[2], t[0], p);
-				eb_add(t[47], t[0], t[45]);
-				eb_add(t[48], t[0], t[46]);
-
-				eb_frb(t[0], t[46]);
-				eb_frb(t[0], t[0]);
-				eb_neg(t[0], t[0]);
-				eb_sub(t[7], t[0], t[46]);
-				eb_sub(t[8], t[0], t[45]);
-				eb_frb(t[0], t[0]);
-				if (u == -1) {
-					eb_neg(t[0], t[0]);
-				}
-				eb_sub(t[3], t[0], p);
-
-				eb_frb(t[0], t[1]);
-				eb_frb(t[0], t[0]);
-				eb_neg(t[0], t[0]);
-				eb_add(t[55], t[0], t[45]);
-				eb_add(t[56], t[0], t[46]);
-				eb_sub(t[9], t[0], p);
-				eb_add(t[10], t[0], p);
-				eb_frb(t[0], t[0]);
-				if (u == 1) {
-					eb_neg(t[0], t[0]);
-				}
-				eb_add(t[4], t[0], p);
-
-				eb_frb(t[0], t[47]);
-				eb_frb(t[0], t[0]);
-				eb_neg(t[0], t[0]);
-				eb_sub(t[17], t[0], p);
-				eb_add(t[18], t[0], p);
-
-				eb_frb(t[0], t[8]);
-				eb_frb(t[0], t[0]);
-				eb_add(t[31], t[0], t[45]);
-				eb_add(t[32], t[0], t[46]);
-				eb_neg(t[0], t[0]);
-				eb_sub(t[13], t[0], p);
-				eb_add(t[14], t[0], p);
-				eb_frb(t[0], t[0]);
-				if (u == -1) {
-					eb_neg(t[0], t[0]);
-				}
-				eb_sub(t[19], t[0], p);
-
-				eb_frb(t[0], t[7]);
-				eb_frb(t[0], t[0]);
-				eb_frb(t[0], t[0]);
-				if (u == -1) {
-					eb_neg(t[0], t[0]);
-				}
-				eb_add(t[20], t[0], p);
-
-				eb_frb(t[0], t[9]);
-				eb_frb(t[0], t[0]);
-				eb_sub(t[21], t[0], p);
-				eb_add(t[22], t[0], p);
-
-				eb_frb(t[0], t[2]);
-				eb_frb(t[0], t[0]);
-				eb_sub(t[25], t[0], p);
-				eb_add(t[26], t[0], p);
-
-				eb_frb(t[0], t[43]);
-				eb_frb(t[0], t[0]);
-				eb_neg(t[0], t[0]);
-				eb_sub(t[33], t[0], p);
-				eb_add(t[34], t[0], p);
-				eb_frb(t[0], t[0]);
-				if (u == -1) {
-					eb_neg(t[0], t[0]);
-				}
-				eb_add(t[12], t[0], p);
-
-				eb_frb(t[0], t[45]);
-				eb_frb(t[0], t[0]);
-				eb_sub(t[37], t[0], p);
-				eb_add(t[38], t[0], p);
-				eb_frb(t[0], t[0]);
-				if (u == -1) {
-					eb_neg(t[0], t[0]);
-				}
-				eb_sub(t[35], t[0], p);
-				eb_add(t[36], t[0], p);
-
-				eb_frb(t[0], t[38]);
-				eb_frb(t[0], t[0]);
-				eb_sub(t[41], t[0], p);
-				eb_add(t[42], t[0], p);
-				eb_neg(t[0], t[0]);
-				eb_sub(t[39], t[0], t[46]);
-				eb_sub(t[40], t[0], t[45]);
-
-				eb_frb(t[0], t[37]);
-				eb_frb(t[0], t[0]);
-				eb_sub(t[5], t[0], p);
-				eb_add(t[6], t[0], p);
-				eb_frb(t[0], t[0]);
-				if (u == -1) {
-					eb_neg(t[0], t[0]);
-				}
-				eb_add(t[15], t[0], t[43]);
-				eb_add(t[16], t[0], t[44]);
-				eb_neg(t[0], t[0]);
-				eb_sub(t[27], t[0], p);
-				eb_add(t[28], t[0], p);
-
-				eb_frb(t[0], t[36]);
-				eb_frb(t[0], t[0]);
-				eb_frb(t[0], t[0]);
-				if (u == -1) {
-					eb_neg(t[0], t[0]);
-				}
-				eb_sub(t[11], t[0], p);
-
-				eb_frb(t[0], t[39]);
-				eb_frb(t[0], t[0]);
-				eb_add(t[0], t[0], p);
-				eb_neg(t[49], t[0]);
-
-				eb_frb(t[0], t[7]);
-				eb_frb(t[0], t[0]);
-				eb_neg(t[0], t[0]);
-				eb_add(t[50], t[0], p);
-
-				eb_frb(t[0], t[44]);
-				eb_frb(t[0], t[0]);
-				eb_frb(t[0], t[0]);
-				if (u == 1) {
-					eb_neg(t[0], t[0]);
-				}
-				eb_sub(t[51], t[0], p);
-				eb_add(t[52], t[0], p);
-
-				eb_frb(t[0], t[46]);
-				eb_frb(t[0], t[0]);
-				eb_neg(t[0], t[0]);
-				eb_sub(t[53], t[0], p);
-				eb_add(t[54], t[0], p);
-
-				eb_frb(t[0], t[54]);
-				eb_frb(t[0], t[0]);
-				eb_add(t[23], t[0], t[45]);
-				eb_add(t[24], t[0], t[46]);
-
-				eb_frb(t[0], t[42]);
-				eb_frb(t[0], t[0]);
-				eb_sub(t[57], t[0], p);
-
-				eb_frb(t[0], t[53]);
-				eb_frb(t[0], t[0]);
-				eb_neg(t[0], t[0]);
-				eb_add(t[58], t[0], p);
-
-				eb_frb(t[0], t[38]);
-				eb_frb(t[0], t[0]);
-				eb_frb(t[0], t[0]);
-				if (u == -1) {
-					eb_neg(t[0], t[0]);
-				}
-				eb_sub(t[59], t[0], p);
-				eb_add(t[60], t[0], p);
-
-				eb_frb(t[0], t[35]);
-				eb_frb(t[0], t[0]);
-				eb_sub(t[61], t[0], p);
-				eb_add(t[62], t[0], p);
-
-				eb_frb(t[0], t[47]);
-				eb_frb(t[0], t[0]);
-				eb_neg(t[0], t[0]);
-				eb_add(t[63], t[0], t[45]);
-
-				eb_frb(t[0], t[36]);
-				eb_frb(t[0], t[0]);
-				eb_neg(t[0], t[0]);
-				eb_sub(t[29], t[0], p);
-				eb_add(t[30], t[0], p);
-
+				eb_neg(t[7], t[37]);
+				eb_add(t[7], t[7], t[0]);
+				eb_add(t[51], t[6], t[0]);
+				eb_add(t[52], t[7], t[0]);
+				eb_neg(t[31], t[51]);
+				eb_sub(t[31], t[31], t[0]);
+				eb_neg(t[30], t[52]);
+				eb_sub(t[30], t[30], t[0]);
+				eb_neg(t[13], t[31]);
+				eb_add(t[13], t[13], t[0]);
+				eb_neg(t[14], t[30]);
+				eb_add(t[14], t[14], t[0]);
+				eb_add(t[58], t[13], t[0]);
+				eb_add(t[59], t[14], t[0]);
+				eb_neg(t[24], t[58]);
+				eb_sub(t[24], t[24], t[0]);
+				eb_neg(t[23], t[59]);
+				eb_sub(t[23], t[23], t[0]);
+				eb_add(t[46], t[57], p);
+				eb_sub(t[1], t[46], t[0]);
+				eb_neg(t[36], t[46]);
+				eb_sub(t[36], t[36], t[0]);
+				eb_neg(t[43], t[1]);
+				eb_add(t[43], t[43], t[0]);
+				eb_neg(t[8], t[36]);
+				eb_add(t[8], t[8], t[0]);
+				eb_neg(t[39], t[43]);
+				eb_sub(t[39], t[39], t[0]);
+				eb_add(t[53], t[8], t[0]);
+				eb_neg(t[5], t[39]);
+				eb_add(t[5], t[5], t[0]);
+				eb_neg(t[29], t[53]);
+				eb_sub(t[29], t[29], t[0]);
+				eb_add(t[50], t[5], t[0]);
+				eb_neg(t[15], t[29]);
+				eb_add(t[15], t[15], t[0]);
+				eb_neg(t[32], t[50]);
+				eb_sub(t[32], t[32], t[0]);
+				eb_add(t[60], t[15], t[0]);
+				eb_neg(t[12], t[32]);
+				eb_add(t[12], t[12], t[0]);
+				eb_neg(t[22], t[60]);
+				eb_sub(t[22], t[22], t[0]);
+				eb_add(t[47], t[1], t[57]);
+				eb_sub(t[2], t[47], t[0]);
+				eb_neg(t[35], t[47]);
+				eb_sub(t[35], t[35], t[0]);
+				eb_neg(t[42], t[2]);
+				eb_add(t[42], t[42], t[0]);
+				eb_neg(t[9], t[35]);
+				eb_add(t[9], t[9], t[0]);
+				eb_neg(t[40], t[42]);
+				eb_sub(t[40], t[40], t[0]);
+				eb_add(t[54], t[9], t[0]);
+				eb_neg(t[4], t[40]);
+				eb_add(t[4], t[4], t[0]);
+				eb_neg(t[28], t[54]);
+				eb_sub(t[28], t[28], t[0]);
+				eb_neg(t[16], t[28]);
+				eb_add(t[16], t[16], t[0]);
+				eb_add(t[61], t[16], t[0]);
+				eb_neg(t[21], t[61]);
+				eb_sub(t[21], t[21], t[0]);
+				eb_add(t[48], t[2], t[57]);
+				eb_sub(t[3], t[48], t[0]);
+				eb_neg(t[34], t[48]);
+				eb_sub(t[34], t[34], t[0]);
+				eb_neg(t[41], t[3]);
+				eb_add(t[41], t[41], t[0]);
+				eb_neg(t[10], t[34]);
+				eb_add(t[10], t[10], t[0]);
+				eb_add(t[55], t[10], t[0]);
+				eb_neg(t[27], t[55]);
+				eb_sub(t[27], t[27], t[0]);
+				eb_neg(t[17], t[27]);
+				eb_add(t[17], t[17], t[0]);
+				eb_add(t[62], t[17], t[0]);
+				eb_neg(t[20], t[62]);
+				eb_sub(t[20], t[20], t[0]);
+				eb_add(t[49], t[3], t[57]);
+				eb_neg(t[33], t[49]);
+				eb_sub(t[33], t[33], t[0]);
+				eb_neg(t[11], t[33]);
+				eb_add(t[11], t[11], t[0]);
+				eb_add(t[56], t[11], t[0]);
+				eb_neg(t[26], t[56]);
+				eb_sub(t[26], t[26], t[0]);
+				eb_neg(t[18], t[26]);
+				eb_add(t[18], t[18], t[0]);
+				eb_add(t[63], t[18], t[0]);
+				eb_add(t[57], t[11], t[57]);
+				eb_neg(t[25], t[57]);
+				eb_sub(t[25], t[25], t[0]);
+				eb_neg(t[19], t[25]);
+				eb_add(t[19], t[19], t[0]);
 				eb_copy(t[0], p);
 				break;
 #endif
