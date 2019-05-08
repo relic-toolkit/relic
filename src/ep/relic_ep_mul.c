ccb135f8c09c3957e4d0237f7a1455c8d525d1f5
Diego F. Aranha <dfaranha@gmail.com>
2019-05-08 18:57:32 +0200

    Add new implementations for LWREG.

--- src/ep/relic_ep_mul.c
+++ src/ep/relic_ep_mul.c
@@ -214,13 +214,165 @@ static void ep_mul_naf_imp(ep_t r, const ep_t p, const bn_t k) {
 
 #if EP_MUL == LWREG || !defined(STRIP)
 
+#if defined(EP_ENDOM)
+
+static void ep_mul_reg_glv(ep_t r, const ep_t p, const bn_t k) {
+	int i, j, l, n0, n1, s0, s1, b0, b1;
+	int8_t _s0, _s1, reg0[RLC_FP_BITS + 1], reg1[RLC_FP_BITS + 1];
+	bn_t n, k0, k1, v1[3], v2[3];
+	ep_t q, t[1 << (EP_WIDTH - 2)], u, v, w;
+
+	if (bn_is_zero(k)) {
+		ep_set_infty(r);
+		return;
+	}
+
+	bn_null(n);
+	bn_null(k0);
+	bn_null(k1);
+	ep_null(q);
+	ep_null(u);
+	ep_null(v);
+	ep_null(w);
+
+	TRY {
+		bn_new(n);
+		bn_new(k0);
+		bn_new(k1);
+		ep_new(q);
+		ep_new(u);
+		ep_new(v);
+		ep_new(w);
+
+		for (i = 0; i < (1 << (EP_WIDTH - 2)); i++) {
+			ep_null(t[i]);
+			ep_new(t[i]);
+		}
+		for (i = 0; i < 3; i++) {
+			bn_null(v1[i]);
+			bn_null(v2[i]);
+			bn_new(v1[i]);
+			bn_new(v2[i]);
+		}
+
+		ep_curve_get_ord(n);
+		ep_curve_get_v1(v1);
+		ep_curve_get_v2(v2);
+		bn_rec_glv(k0, k1, k, n, (const bn_t *)v1, (const bn_t *)v2);
+		s0 = bn_sign(k0);
+		s1 = bn_sign(k1);
+		bn_abs(k0, k0);
+		bn_abs(k1, k1);
+		b0 = bn_is_even(k0);
+		b1 = bn_is_even(k1);
+		k0->dp[0] |= b0;
+		k1->dp[0] |= b1;
+
+		ep_copy(q, p);
+		ep_neg(t[0], p);
+		dv_copy_cond(q->y, t[0]->y, RLC_FP_DIGS, s0 != RLC_POS);
+		ep_tab(t, q, EP_WIDTH);
+
+		l = RLC_FP_BITS + 1;
+		bn_rec_reg(reg0, &l, k0, bn_bits(n)/2, EP_WIDTH);
+		l = RLC_FP_BITS + 1;
+		bn_rec_reg(reg1, &l, k1, bn_bits(n)/2, EP_WIDTH);
+
+#if defined(EP_MIXED)
+		fp_set_dig(u->z, 1);
+		fp_set_dig(w->z, 1);
+		u->norm = w->norm = 1;
+#else
+		u->norm = w->norm = 0;
+#endif
+		ep_set_infty(r);
+		for (i = l - 1; i >= 0; i--) {
+			for (j = 0; j < EP_WIDTH - 1; j++) {
+				ep_dbl(r, r);
+			}
+
+			n0 = reg0[i];
+			_s0 = (n0 >> 7);
+			n0 = ((n0 ^ _s0) - _s0) >> 1;
+			n1 = reg1[i];
+			_s1 = (n1 >> 7);
+			n1 = ((n1 ^ _s1) - _s1) >> 1;
+
+			for (j = 0; j < (1 << (EP_WIDTH - 2)); j++) {
+				dv_copy_cond(u->x, t[j]->x, RLC_FP_DIGS, j == n0);
+				dv_copy_cond(w->x, t[j]->x, RLC_FP_DIGS, j == n1);
+				dv_copy_cond(u->y, t[j]->y, RLC_FP_DIGS, j == n0);
+				dv_copy_cond(w->y, t[j]->y, RLC_FP_DIGS, j == n1);
+#if !defined(EP_MIXED)
+				dv_copy_cond(u->z, t[j]->z, RLC_FP_DIGS, j == n0);
+				dv_copy_cond(w->z, t[j]->z, RLC_FP_DIGS, j == n1);
+#endif
+			}
+			ep_neg(v, u);
+			dv_copy_cond(u->y, v->y, RLC_FP_DIGS, _s0 != 0);
+			ep_add(r, r, u);
+
+			fp_mul(w->x, w->x, ep_curve_get_beta());
+			ep_neg(q, w);
+			dv_copy_cond(w->y, q->y, RLC_FP_DIGS, s0 != s1);
+			ep_neg(q, w);
+			dv_copy_cond(w->y, q->y, RLC_FP_DIGS, _s1 != 0);
+			ep_add(r, r, w);
+		}
+
+		/* t[0] has an unmodified copy of p. */
+		ep_sub(u, r, t[0]);
+		dv_copy_cond(r->x, u->x, RLC_FP_DIGS, b0);
+		dv_copy_cond(r->y, u->y, RLC_FP_DIGS, b0);
+		dv_copy_cond(r->z, u->z, RLC_FP_DIGS, b0);
+
+		ep_copy(w, t[0]);
+		fp_mul(w->x, w->x, ep_curve_get_beta());
+		ep_neg(q, w);
+		dv_copy_cond(w->y, q->y, RLC_FP_DIGS, s0 != s1);
+		ep_sub(u, r, w);
+		dv_copy_cond(r->x, u->x, RLC_FP_DIGS, b1);
+		dv_copy_cond(r->y, u->y, RLC_FP_DIGS, b1);
+		dv_copy_cond(r->z, u->z, RLC_FP_DIGS, b1);
+
+		/* Convert r to affine coordinates. */
+		ep_norm(r, r);
+		ep_neg(u, r);
+		dv_copy_cond(r->y, u->y, RLC_FP_DIGS, bn_sign(k) == RLC_NEG);
+	}
+	CATCH_ANY {
+		THROW(ERR_CAUGHT);
+	}
+	FINALLY {
+		bn_free(n);
+		bn_free(k0);
+		bn_free(k1);
+		bn_free(n)
+		ep_free(q);
+		ep_free(u);
+		ep_free(v);
+		ep_free(w);
+		for (i = 0; i < 1 << (EP_WIDTH - 2); i++) {
+			ep_free(t[i]);
+		}
+		for (i = 0; i < 3; i++) {
+			bn_free(v1[i]);
+			bn_free(v2[i]);
+		}
+	}
+}
+
+#endif /* EP_ENDOM */
+
 #if defined(EP_PLAIN) || defined(EP_SUPER)
 
 static void ep_mul_reg_imp(ep_t r, const ep_t p, const bn_t k) {
+	bn_t _k;
 	int i, j, l, n;
-	int8_t reg[RLC_CEIL(RLC_FP_BITS + 1, EP_WIDTH - 1)], *_k;
-	ep_t t[1 << (EP_WIDTH - 2)];
+	int8_t s, reg[RLC_CEIL(RLC_FP_BITS + 1, EP_WIDTH - 1)];
+	ep_t t[1 << (EP_WIDTH - 2)], u, v;
 
+	bn_null(_k);
 	for (i = 0; i < (1 << (EP_WIDTH - 2)); i++) {
 		ep_null(t[i]);
 	}
@@ -231,6 +383,9 @@ static void ep_mul_reg_imp(ep_t r, const ep_t p, const bn_t k) {
 	}
 
 	TRY {
+		bn_new(_k);
+		ep_new(u);
+		ep_new(v);
 		/* Prepare the precomputation table. */
 		for (i = 0; i < (1 << (EP_WIDTH - 2)); i++) {
 			ep_new(t[i]);
@@ -238,29 +393,53 @@ static void ep_mul_reg_imp(ep_t r, const ep_t p, const bn_t k) {
 		/* Compute the precomputation table. */
 		ep_tab(t, p, EP_WIDTH);
 
-		/* Compute the w-NAF representation of k. */
+		ep_curve_get_ord(_k);
+		n = bn_bits(_k);
+
+		/* Make a copy of the scalar for processing. */
+		bn_abs(_k, k);
+		_k->dp[0] |= bn_is_even(_k);
+
+		/* Compute the regular w-NAF representation of k. */
 		l = RLC_CEIL(RLC_FP_BITS + 1, EP_WIDTH - 1);
-		bn_rec_reg(reg, &l, k, RLC_FP_BITS, EP_WIDTH);
+		bn_rec_reg(reg, &l, _k, n, EP_WIDTH);
 
-		_k = reg + l - 1;
-
+#if defined(EP_MIXED)
+		fp_set_dig(u->z, 1);
+		u->norm = 1;
+#else
+		u->norm = 0;
+#endif
 		ep_set_infty(r);
-		for (i = l - 1; i >= 0; i--, _k--) {
+		for (i = l - 1; i >= 0; i--) {
 			for (j = 0; j < EP_WIDTH - 1; j++) {
 				ep_dbl(r, r);
 			}
 
-			n = *_k;
-			if (n > 0) {
-				ep_add(r, r, t[n / 2]);
-			}
-			if (n < 0) {
-				ep_sub(r, r, t[-n / 2]);
-			}
-		}
+			n = reg[i];
+			s = (n >> 7);
+			n = ((n ^ s) - s) >> 1;
 
+			for (j = 0; j < (1 << (EP_WIDTH - 2)); j++) {
+				dv_copy_cond(u->x, t[j]->x, RLC_FP_DIGS, j == n);
+				dv_copy_cond(u->y, t[j]->y, RLC_FP_DIGS, j == n);
+#if !defined(EP_MIXED)
+				dv_copy_cond(u->z, t[j]->z, RLC_FP_DIGS, j == n);
+#endif
+			}
+			ep_neg(v, u);
+			dv_copy_cond(u->y, v->y, RLC_FP_DIGS, s != 0);
+			ep_add(r, r, u);
+		}
+		/* t[0] has an unmodified copy of p. */
+		ep_sub(u, r, t[0]);
+		dv_copy_cond(r->x, u->x, RLC_FP_DIGS, bn_is_even(k));
+		dv_copy_cond(r->y, u->y, RLC_FP_DIGS, bn_is_even(k));
+		dv_copy_cond(r->z, u->z, RLC_FP_DIGS, bn_is_even(k));
 		/* Convert r to affine coordinates. */
 		ep_norm(r, r);
+		ep_neg(u, r);
+		dv_copy_cond(r->y, u->y, RLC_FP_DIGS, bn_sign(k) == RLC_NEG);
 	}
 	CATCH_ANY {
 		THROW(ERR_CAUGHT);
@@ -270,6 +449,9 @@ static void ep_mul_reg_imp(ep_t r, const ep_t p, const bn_t k) {
 		for (i = 0; i < (1 << (EP_WIDTH - 2)); i++) {
 			ep_free(t[i]);
 		}
+		bn_free(_k);
+		ep_free(u);
+		ep_free(v);
 	}
 }
 
@@ -467,7 +649,7 @@ void ep_mul_lwreg(ep_t r, const ep_t p, const bn_t k) {
 
 #if defined(EP_ENDOM)
 	if (ep_curve_is_endom()) {
-		ep_mul_glv_imp(r, p, k);
+		ep_mul_reg_glv(r, p, k);
 		return;
 	}
 #endif
