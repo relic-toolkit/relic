ed2a9800276bc3d105eada19858fe571926f13b9
Diego F. Aranha <dfaranha@gmail.com>
2019-07-16 11:37:14 +0200

    Update to fix coding standard and attempt to extend support to DYNAMIC.

--- include/relic_alloc.h
+++ include/relic_alloc.h
@@ -1,23 +1,64 @@
+/*
+ * RELIC is an Efficient LIbrary for Cryptography
+ * Copyright (C) 2007-2019 RELIC Authors
+ *
+ * This file is part of RELIC. RELIC is legal property of its developers,
+ * whose names are not listed here. Please refer to the COPYRIGHT file
+ * for contact information.
+ *
+ * RELIC is free software; you can redistribute it and/or modify it under the
+ * terms of the version 2.1 (or later) of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; or version 2.0 of the Apache
+ * License as published by the Apache Software Foundation. See the LICENSE files
+ * for more details.
+ *
+ * RELIC is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the LICENSE files for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public or the
+ * Apache License along with RELIC. If not, see <https://www.gnu.org/licenses/>
+ * or <https://www.apache.org/licenses/>.
+ */
 
-
+/**
+ * @file
+ *
+ * Implementation of the auxiliary memory allocation functions.
+ *
+ * @ingroup utils
+ */
 
 #ifdef _MSC_VER
     #include <malloc.h>
 
-    // Dynamiclly allocates an array of "Type" with the specified size on the stack. 
-    // This memory will be automaticlly deallocated from the stack when the function 
-    // frame is returned from.
-    //
-    // Note: This is the Windows specific implementation.
-    #define RLC_ALLOCA(Type, size)(Type*) _alloca((size) * sizeof(Type))
+    /* Dynamiclly allocates an array of "Type" with the specified size on the stack.
+     * This memory will be automaticlly deallocated from the stack when the function
+     * frame is returned from.
+     * Note: This is the Windows specific implementation.
+     *
+     * @param[in] T                 - the type of each object.
+     * @param[in] S                 - the number of obecs to allocate.
+     */
+    #if ALLOC == DYNAMIC
+        #define RLC_ALLOCA(T, S)(T*) malloc((S) * sizeof(T))
+    #else
+        #define RLC_ALLOCA(T, S)(T*) _alloca((S) * sizeof(T))
+    #endif
 #else
-
     #include <alloca.h>
 
-    // Dynamiclly allocates an array of "Type" with the specified size on the stack. 
-    // This memory will be automaticlly deallocated from the stack when the function 
-    // frame is returned from. 
-    //
-    // Note: This is the POSIX specific implementation.
-    #define RLC_ALLOCA(Type, size)(Type*) alloca((size) * sizeof(Type))
+    /* Dynamiclly allocates an array of "Type" with the specified size on the stack.
+     * This memory will be automaticlly deallocated from the stack when the function
+     * frame is returned from.
+     * Note: This is the POSIX specific implementation.
+     *
+     * @param[in] T                 - the type of each object.
+     * @param[in] S                 - the number of obecs to allocate.
+     */
+    #if ALLOC == DYNAMIC
+        #define RLC_ALLOCA(T, S)(T*) malloc((S) * sizeof(T))
+    #else
+        #define RLC_ALLOCA(T, S)(T*) alloca((S) * sizeof(T))
+    #endif
 #endif
