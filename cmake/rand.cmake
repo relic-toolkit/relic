e6721c799bbd0ad50764c0a9f76ef3423bba93ca
Diego F. Aranha <dfaranha@gmail.com>
2020-01-21 11:44:08 +0100

    Refactor for clarity.

--- cmake/rand.cmake
+++ cmake/rand.cmake
@@ -1,16 +1,16 @@
 message(STATUS "Available pseudo-random number generators (default = HASHD):\n")
 
 message("   RAND=HASHD     Use the HASH-DRBG generator. (recommended)")
+message("   RAND=RDRND     Use Intel RdRand instruction directly.")
 message("   RAND=UDEV      Use the operating system underlying generator.")
-message("   RAND=RDRND     Use Intel RdRand instruction directly.\n")
 message("   RAND=CALL      Override the generator with a callback.\n")
 
 message(STATUS "Available random number generator seeders (default = UDEV):\n")
 
 message("   SEED=          Use a zero seed. (horribly insecure!)")
 message("   SEED=LIBC      Use rand()/random() functions. (insecure!)")
-message("   SEED=UDEV      Use non-blocking /dev/urandom. (recommended)")
 message("   SEED=RDRND     Use Intel RdRand instruction directly.")
+message("   SEED=UDEV      Use non-blocking /dev/urandom. (recommended)")
 message("   SEED=WCGR      Use Windows' CryptGenRandom. (recommended)\n")
 
 # Choose the pseudo-random number generator.
