12d5e6c42ad286d891ca83be0300ef0ee9e176e6
Diego F. Aranha <dfaranha@gmail.com>
2020-01-21 11:44:39 +0100

    Remove SHA1 and refactor PRNG configuration.

--- include/relic_md.h
+++ include/relic_md.h
@@ -45,8 +45,6 @@
 /*============================================================================*/
 
 enum {
-	/** Hash length for SHA-1 function. */
-	RLC_MD_LEN_SHONE = 20,
 	/** Hash length for SHA-224 function. */
 	RLC_MD_LEN_SH224 = 28,
 	/** Hash length for SHA-256 function. */
@@ -64,9 +62,7 @@ enum {
 /**
  * Length in bytes of default hash function output.
  */
-#if MD_MAP == SHONE
-#define RLC_MD_LEN					RLC_MD_LEN_SHONE
-#elif MD_MAP == SH224
+#if MD_MAP == SH224
 #define RLC_MD_LEN					RLC_MD_LEN_SH224
 #elif MD_MAP == SH256
 #define RLC_MD_LEN					RLC_MD_LEN_SH256
@@ -92,9 +88,7 @@ enum {
  * @param[in] M					- the message to hash.
  * @param[in] L					- the message length in bytes.
  */
-#if MD_MAP == SHONE
-#define md_map(H, M, L)			md_map_shone(H, M, L)
-#elif MD_MAP == SH224
+#if MD_MAP == SH224
 #define md_map(H, M, L)			md_map_sh224(H, M, L)
 #elif MD_MAP == SH256
 #define md_map(H, M, L)			md_map_sh256(H, M, L)
@@ -112,23 +106,6 @@ enum {
 /* Function prototypes                                                        */
 /*============================================================================*/
 
-/**
- * Computes the SHA-1 hash function.
- *
- * @param[out] hash				- the digest.
- * @param[in] msg				- the message to hash.
- * @param[in] len				- the message length in bytes.
- */
-void md_map_shone(uint8_t *hash, const uint8_t *msg, int len);
-
-/**
- * Returns the internal state of the hash function.
- *
- * @param[out] state			- the internal state.
- * @param[in] msg				- the message to hash.
- * @param[in] len				- the message length in bytes.
- */
-void md_map_shone_mid(uint8_t *state, uint8_t *msg, int len);
 /**
  * Computes the SHA-224 hash function.
  *
@@ -184,17 +161,6 @@ void md_map_b2s160(uint8_t *hash, const uint8_t *msg, int len);
 void md_map_b2s256(uint8_t *hash, const uint8_t *msg, int len);
 
 /**
- * Derives a key from shared secret material through the standardized KDF1
- * function.
- *
- * @param[out] key				- the resulting key.
- * @param[in] key_len			- the intended key length in bytes.
- * @param[in] in				- the shared secret.
- * @param[in] in_len			- the length of the shared secret in bytes.
- */
-void md_kdf1(uint8_t *key, int key_len, const uint8_t *in, int in_len);
-
-/**
  * Derives a key from shared secret material through the standardized KDF2
  * function.
  *
@@ -203,18 +169,18 @@ void md_kdf1(uint8_t *key, int key_len, const uint8_t *in, int in_len);
  * @param[in] in				- the shared secret.
  * @param[in] in_len			- the length of the shared secret in bytes.
  */
-void md_kdf2(uint8_t *key, int key_len, const uint8_t *in, int in_len);
+void md_kdf(uint8_t *key, int key_len, const uint8_t *in, int in_len);
 
 /**
- * Derives a mask from shared secret material through the PKCS#1 2.0 MGF1
- * function .
+ * Derives a mask from shared secret material through the PKCS#1 2.1 MGF1
+ * function. This is the same as the standardized KDF1 key derivation function.
  *
  * @param[out] key				- the resulting mask.
  * @param[in] key_len			- the intended mask length in bytes.
  * @param[in] in				- the shared secret.
  * @param[in] in_len			- the length of the shared secret in bytes.
  */
-void md_mgf1(uint8_t *mask, int mask_len, const uint8_t *in, int in_len);
+void md_mgf(uint8_t *mask, int mask_len, const uint8_t *in, int in_len);
 
 /**
  * Computes a Message Authentication Code through HMAC.
