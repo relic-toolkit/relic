4e010d5fcbabc8310d6389bbde7809084d6e8973
Diego F. Aranha <dfaranha@gmail.com>
2020-02-17 15:56:28 +0100

    Add present for fiat backend.

--- preset/fiat-pbc-bls381.sh
+++ preset/fiat-pbc-bls381.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-cmake -DCHECK=off -DARITH=fiat -DFP_PRIME=381 -DFP_QNRES=on -DFP_METHD="BASIC;COMBA;COMBA;MONTY;LOWER;SLIDE" -DFPX_METHD="INTEG;INTEG;LAZYR" -DPP_METHD="LAZYR;OATEP" -DCOMP="-O3 -funroll-loops -fomit-frame-pointer -finline-small-functions -march=native -mtune=native" $1
+cmake -DCHECK=off -DARITH=x64-fiat-381 -DFP_PRIME=381 -DFP_QNRES=on -DFP_METHD="BASIC;COMBA;COMBA;MONTY;LOWER;SLIDE" -DFPX_METHD="INTEG;INTEG;LAZYR" -DPP_METHD="LAZYR;OATEP" -DCOMP="-O3 -funroll-loops -fomit-frame-pointer -finline-small-functions -march=native -mtune=native" $1
