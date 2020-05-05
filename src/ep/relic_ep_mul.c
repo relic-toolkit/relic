6109db59b77843992da09222b1cb3b2a8b60ac4b
Diego F. Aranha <dfaranha@gmail.com>
2020-05-05 10:07:25 +0200

    Improve blinding of accumulators.

--- src/ep/relic_ep_mul.c
+++ src/ep/relic_ep_mul.c
@@ -593,11 +593,14 @@ void ep_mul_slide(ep_t r, const ep_t p, const bn_t k) {
 
 void ep_mul_monty(ep_t r, const ep_t p, const bn_t k) {
 	int i, j, bits;
+	fp_t tmp, rand;
 	ep_t t[2];
 	bn_t n, l;
 
 	bn_null(n);
 	bn_null(l);
+	fp_null(tmp);
+	fp_null(rand);
 	ep_null(t[0]);
 	ep_null(t[1]);
 
@@ -609,6 +612,8 @@ void ep_mul_monty(ep_t r, const ep_t p, const bn_t k) {
 	TRY {
 		bn_new(n);
 		bn_new(l);
+		fp_new(tmp);
+		fp_new(rand);
 		ep_new(t[0]);
 		ep_new(t[1]);
 
@@ -625,11 +630,15 @@ void ep_mul_monty(ep_t r, const ep_t p, const bn_t k) {
 		ep_dbl(t[1], t[0]);
 
 #if EP_ADD == PROJC
-		fp_rand(t[0]->z);
-		fp_sqr(t[0]->x, t[0]->z);
-		fp_mul(t[0]->y, t[0]->x, t[0]->z);
-		fp_mul(t[0]->x, t[0]->x, p->x);
-		fp_mul(t[0]->y, t[0]->y, p->y);
+		/* Blind both points independently. */
+		for (i = 0; i < 2; i++) {
+			fp_rand(rand);
+			fp_mul(t[i]->z, t[i]->z, rand);
+			fp_sqr(tmp, rand);
+			fp_mul(t[i]->x, t[i]->x, tmp);
+			fp_mul(rand, rand, tmp);
+			fp_mul(t[i]->y, t[i]->y, rand);
+		}
 #endif
 
 		for (i = bits - 1; i >= 0; i--) {
@@ -654,6 +663,8 @@ void ep_mul_monty(ep_t r, const ep_t p, const bn_t k) {
 	FINALLY {
 		bn_free(n);
 		bn_free(l);
+		fp_free(tmp);
+		fp_free(rand);
 		ep_free(t[1]);
 		ep_free(t[0]);
 	}
