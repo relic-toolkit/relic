94b363c70d605665cf6138a7196e9a60db5aecfb
Diego F. Aranha <dfaranha@gmail.com>
2020-05-19 16:47:48 +0200

    Added benchmarks for MPC exponentiation in G_T.

--- bench/bench_mpc.c
+++ bench/bench_mpc.c
@@ -90,17 +90,19 @@ static void mul_triple(void) {
 static void pair_triple(void) {
 	g1_t d[2], p[2], _p;
 	g2_t e[2], q[2], _q;
-	gt_t r[2];
+	gt_t f[2], r[2], _r;
 	bn_t k[2], l[2], n;
 	mt_t tri[2];
 	pt_t t[2];
 
 	g1_null(_p);
 	g2_null(_q);
+	gt_null(_r);
 	bn_null(n);
 
 	g1_new(_p);
 	g2_new(_q);
+	gt_new(_r);
 	bn_new(n);
 	for (int j = 0; j < 2; j++) {
 		g1_null(d[j]);
@@ -119,6 +121,7 @@ static void pair_triple(void) {
 		g1_new(p[j]);
 		g2_new(q[j]);
 		gt_new(r[j]);
+		gt_new(f[j]);
 		mt_new(tri[j]);
 		pt_new(t[j]);
 	}
@@ -190,6 +193,38 @@ static void pair_triple(void) {
 		BENCH_ADD(g2_mul_mpc(e[1], l[1], e[1], tri[1], q[1], 1));
 	} BENCH_DIV(2);
 
+	mt_gen(tri, n);
+	BENCH_BEGIN("gt_exp") {
+		/* Generate random inputs. */
+		gt_rand(r[0]);
+		bn_rand_mod(k[0], n);
+		BENCH_ADD(gt_exp(_r, r[0], k[0]));
+	} BENCH_END;
+	/* Secret share inputs. */
+	gt_rand(r[1]);
+	gt_mul(r[0], r[0], r[1]);
+	gt_inv(r[1], r[1]);
+	bn_rand_mod(k[1], n);
+	bn_sub(k[0], k[0], k[1]);
+	if (bn_sign(k[0]) == RLC_NEG) {
+		bn_add(k[0], k[0], n);
+	}
+	bn_mod(k[0], k[0], n);
+
+	BENCH_BEGIN("gt_exp_lcl") {
+		BENCH_ADD(gt_exp_lcl(l[0], f[0], r[0], k[0], r[0], tri[0]));
+		BENCH_ADD(gt_exp_lcl(l[1], f[1], r[1], k[1], r[1], tri[1]));
+	} BENCH_DIV(2);
+
+	BENCH_BEGIN("gt_exp_bct") {
+		BENCH_ADD(gt_exp_bct(l, f));
+	} BENCH_END;
+
+	BENCH_BEGIN("gt_exp_mpc") {
+		BENCH_ADD(gt_exp_mpc(f[0], l[0], f[0], tri[0], r[0], 0));
+		BENCH_ADD(gt_exp_mpc(f[1], l[1], f[1], tri[1], r[1], 1));
+	} BENCH_DIV(2);
+
 	/* Generate random inputs and triple. */
 	pc_map_tri(t);
 	BENCH_BEGIN("pc_map") {
@@ -221,6 +256,7 @@ static void pair_triple(void) {
 
 	g1_free(_p);
 	g2_free(_q);
+	gt_free(_r);
 	bn_free(n);
 	for (int j = 0; j < 2; j++) {
 		g1_free(d[j]);
@@ -230,6 +266,7 @@ static void pair_triple(void) {
 		g1_free(p[j]);
 		g2_free(q[j]);
 		gt_free(r[j]);
+		gt_free(f[j]);
 		pt_free(t[j]);
 		mt_free(tri[j]);
 	}
