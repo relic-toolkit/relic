76e0b754fa2e70eed2caf81d511aa5359349daa0
Diego F. Aranha <dfaranha@gmail.com>
2020-06-01 19:00:34 +0200

    Improve Fiat-crypto support.

--- cmake/fiat.cmake
+++ cmake/fiat.cmake
@@ -1,7 +1,7 @@
 set(TEST_FP "${CMAKE_BINARY_DIR}/../bin/test_fp")
 set(FIAT_TXT "${CMAKE_BINARY_DIR}/test_fp.txt")
 set(FIAT_LOW "${CMAKE_SOURCE_DIR}/../../src/low/fiat/")
-set(FIAT_FP "${FIAT_LOW}/fiat_fp.c")
+set(FIAT_FP "fiat_fp.c")
 set(MONT "/src/ExtractionOCaml/word_by_word_montgomery")
 
 execute_process(COMMAND ${TEST_FP} OUTPUT_FILE ${FIAT_TXT})
@@ -33,6 +33,6 @@ file(READ ${FIAT_FP} OUTPUT_CONTENT)
 string(REPLACE "void" "static void" OUTPUT_FIXED "${OUTPUT_CONTENT}")
 file(WRITE ${FIAT_FP} "${OUTPUT_FIXED}")
 
-execute_process(COMMAND cp ${FIAT_LOW}/relic_fp_add_low.tmpl ${FIAT_LOW}/relic_fp_add_low.c)
-execute_process(COMMAND cp ${FIAT_LOW}/relic_fp_mul_low.tmpl ${FIAT_LOW}/relic_fp_mul_low.c)
-execute_process(COMMAND cp ${FIAT_LOW}/relic_fp_sqr_low.tmpl ${FIAT_LOW}/relic_fp_sqr_low.c)
+configure_file(${FIAT_LOW}/relic_fp_add_low.tmpl ${FIAT_LOW}/relic_fp_add_low.c COPYONLY)
+configure_file(${FIAT_LOW}/relic_fp_mul_low.tmpl ${FIAT_LOW}/relic_fp_mul_low.c COPYONLY)
+configure_file(${FIAT_LOW}/relic_fp_sqr_low.tmpl ${FIAT_LOW}/relic_fp_sqr_low.c COPYONLY)
