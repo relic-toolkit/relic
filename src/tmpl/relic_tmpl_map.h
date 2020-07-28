22b5ab4346b6631e728e3b6cace580e9153e422d
Bill Blanke <wjb2002@flex.com>
2020-07-28 12:55:46 -0700

    updates

--- src/tmpl/relic_tmpl_map.h
+++ src/tmpl/relic_tmpl_map.h
@@ -87,19 +87,6 @@
 		PFX##_mul(q->y, q->y, t3);											\
 		q->coord = PROJC;													\
 	}
-
-/* MSVC does not allow passing empty as macro param, so define new paramless macro */
-#define TMPL_MAP_ISOMAP_NORM_EMPTYPARAM()											\
-	{																\
-		/* Z = Dx * Dy. */													\
-		fp_mul(q->z, t2, t3);										\
-		/* X = Nx * Dy. */													\
-		fp_mul(q->x, t0, t2);										\
-		/* Y = Ny * Dx. */													\
-		fp_mul(q->y, p->y, t1);										\
-		fp_mul(q->y, q->y, t3);										\
-		q->coord = PROJC;													\
-	}
 #else
 #define TMPL_MAP_ISOMAP_NORM(PFX)											\
 	do {																	\
@@ -165,53 +152,6 @@
 			PFX##_free(t1);													\
 			PFX##_free(t2);													\
 			PFX##_free(t3);													\
-		}																	\
-	}
-
-/* MSVC does not allow passing empty as macro param, so define new paramless macro */
-#define TMPL_MAP_ISOGENY_MAP_EMPTYPARAM()											\
-	/* declaring this function inline suppresses unused warnings */			\
-	static inline void ep_iso(ep_t q, ep_t p) {		\
-		fp_t t0, t1, t2, t3;											\
-																			\
-		if (!ep_curve_is_ctmap()) {									\
-			ep_copy(q, p);											\
-			return;															\
-		}																	\
-		/* XXX need to add real support for input projective points */		\
-		if (p->coord != BASIC) {											\
-			ep_norm(p, p);											\
-		}																	\
-																			\
-		fp_null(t0);													\
-		fp_null(t1);													\
-		fp_null(t2);													\
-		fp_null(t3);													\
-                                                                    		\
-		RLC_TRY {																\
-			fp_new(t0);												\
-			fp_new(t1);												\
-			fp_new(t2);												\
-			fp_new(t3);												\
-																			\
-			iso_t coeffs = ep_curve_get_iso();				\
-																			\
-			/* numerators */												\
-			fp_eval(t0, p->x, coeffs->xn, coeffs->deg_xn);			\
-			fp_eval(t1, p->x, coeffs->yn, coeffs->deg_yn);			\
-			/* denominators */												\
-			fp_eval(t2, p->x, coeffs->yd, coeffs->deg_yd);			\
-			fp_eval(t3, p->x, coeffs->xd, coeffs->deg_xd);			\
-																			\
-			/* normalize if necessary, MSVC does not allow passing empty as macro param, so define new paramless macro */		\
-			TMPL_MAP_ISOMAP_NORM_EMPTYPARAM();										\
-		}																	\
-		RLC_CATCH_ANY { RLC_THROW(ERR_CAUGHT); }									\
-		RLC_FINALLY {															\
-			fp_free(t0);												\
-			fp_free(t1);												\
-			fp_free(t2);												\
-			fp_free(t3);												\
 		}																	\
 	}
 
