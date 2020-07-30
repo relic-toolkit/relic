6e8c1ef88426e371ed74ab59fd6120c35583c426
Bill Blanke <wjb2002@flex.com>
2020-07-30 10:27:09 -0700

    bls changes

--- src/ep/relic_ep_pck.c
+++ src/ep/relic_ep_pck.c
@@ -35,6 +35,82 @@
 /* Public definitions                                                         */
 /*============================================================================*/
 
+//[!CHIA_EDIT_START]
+void ep_pck(ep_t r, const ep_t p) {
+	bn_t halfQ;
+	bn_null(halfQ);
+	bn_new(halfQ);
+	halfQ->used = RLC_FP_DIGS;
+	dv_copy(halfQ->dp, fp_prime_get(), RLC_FP_DIGS);
+	bn_hlv(halfQ, halfQ);
+
+	bn_t yValue;
+	bn_null(yValue);
+	bn_new(yValue);
+	fp_prime_back(yValue, p->y);
+
+	int b = bn_cmp(yValue, halfQ) == RLC_GT;
+
+	bn_free(yValue);
+	bn_free(halfQ);
+
+	fp_copy(r->x, p->x);
+	fp_zero(r->y);
+	fp_set_bit(r->y, 0, b);
+	fp_set_dig(r->z, 1);
+	r->coord = BASIC;
+}
+
+int ep_upk(ep_t r, const ep_t p) {
+	fp_t t;
+	bn_t halfQ;
+	bn_t yValue;
+	int result = 0;
+
+	fp_null(t);
+	bn_null(halfQ);
+	bn_null(yValue);
+
+	RLC_TRY {
+		fp_new(t);
+
+		ep_rhs(t, p);
+
+		/* t0 = sqrt(x1^3 + a * x1 + b). */
+		result = fp_srt(t, t);
+
+		if (result) {
+			/* Verify whether the y coordinate is the larger one, matches the
+			 * compressed y-coordinate. */
+			bn_new(halfQ);
+			halfQ->used = RLC_FP_DIGS;
+			dv_copy(halfQ->dp, fp_prime_get(), RLC_FP_DIGS);
+			bn_hlv(halfQ, halfQ);
+
+			bn_new(yValue);
+			fp_prime_back(yValue, t);
+			int b = bn_cmp(yValue, halfQ) == RLC_GT;
+
+			if (b != fp_get_bit(p->y, 0)) {
+				fp_neg(t, t);
+			}
+			fp_copy(r->x, p->x);
+			fp_copy(r->y, t);
+			fp_set_dig(r->z, 1);
+			r->coord = BASIC;
+		}
+	}
+	RLC_CATCH_ANY {
+		RLC_THROW(ERR_CAUGHT);
+	}
+	RLC_FINALLY {
+		fp_free(t);
+		bn_free(yValue);
+		bn_free(halfQ);
+	}
+	return result;
+}
+/*
 void ep_pck(ep_t r, const ep_t p) {
 	int b = fp_get_bit(p->y, 0);
 	fp_copy(r->x, p->x);
@@ -55,12 +131,12 @@ int ep_upk(ep_t r, const ep_t p) {
 
 		ep_rhs(t, p);
 
-		/* t0 = sqrt(x1^3 + a * x1 + b). */
+		// t0 = sqrt(x1^3 + a * x1 + b).
 		result = fp_srt(t, t);
 
 		if (result) {
-			/* Verify if least significant bit of the result matches the
-			 * compressed y-coordinate. */
+			// Verify if least significant bit of the result matches the
+			// compressed y-coordinate.
 			if (fp_get_bit(t, 0) != fp_get_bit(p->y, 0)) {
 				fp_neg(t, t);
 			}
@@ -78,3 +154,5 @@ int ep_upk(ep_t r, const ep_t p) {
 	}
 	return result;
 }
+*/
+//[!CHIA_EDIT_END]
--- src/relic_core.c
+++ src/relic_core.c
@@ -82,9 +82,16 @@ rlc_thread ctx_t first_ctx;
 static ctx_t first_ctx;
 #endif
 
-/**
- * Active library context.
+//[!CHIA_EDIT_START]
+#if MULTI != RELIC_NONE
+/*
+ * Initializer function to call for every thread's context
  */
+void (*core_thread_initializer)(void* init_ptr) = NULL;
+void* core_init_ptr = NULL;
+#endif
+//[!CHIA_EDIT_END]
+
 #if MULTI
 rlc_thread ctx_t *core_ctx = NULL;
 #else
@@ -175,9 +182,25 @@ int core_clean(void) {
 }
 
 ctx_t *core_get(void) {
+//[!CHIA_EDIT_START]
+#if MULTI != RELIC_NONE
+    if (core_ctx == NULL && core_thread_initializer != NULL) {
+        core_thread_initializer(core_init_ptr);
+    }
+#endif
+//[!CHIA_EDIT_END]
 	return core_ctx;
 }
 
 void core_set(ctx_t *ctx) {
 	core_ctx = ctx;
 }
+
+//[!CHIA_EDIT_START]
+#if MULTI != RELIC_NONE
+void core_set_thread_initializer(void(*init)(void *init_ptr), void* init_ptr) {
+    core_thread_initializer = init;
+    core_init_ptr = init_ptr;
+}
+#endif
+//[!CHIA_EDIT_END]
