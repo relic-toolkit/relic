4d03fcc21f9938153f8032a6ef74306ffb732232
Diego F. Aranha <dfaranha@gmail.com>
2021-01-19 10:27:34 +0100

    Stop reproducing CMake build stuff and reuse that instead.

--- preset/gmp-paillier-4096.sh
+++ preset/gmp-paillier-4096.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-cmake -DCHECK=off -DARITH=gmp -DBN_PRECI=4096 -DALLOC=DYNAMIC -DCOMP="-O3 -march=native -mtune=native -fomit-frame-pointer" -DWITH="DV;BN;MD;CP" -DSHLIB=off $1
+cmake -DCHECK=off -DARITH=gmp -DBN_PRECI=4096 -DALLOC=DYNAMIC -DCFLAGS="-O3 -march=native -mtune=native -fomit-frame-pointer" -DWITH="DV;BN;MD;CP" -DSHLIB=off $1
