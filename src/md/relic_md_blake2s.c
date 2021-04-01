3a23142be0a5510a3aa93cd6c76fc59d3fc732a5
Diego F. Aranha <dfaranha@gmail.com>
2021-04-01 11:39:09 +0200

    Bump BLAKE2S to latest version.

--- src/md/relic_md_blake2s.c
+++ src/md/relic_md_blake2s.c
@@ -45,7 +45,7 @@
 
 void md_map_b2s160(uint8_t *hash, const uint8_t *msg, int len) {
 	memset(hash, 0, RLC_MD_LEN_B2S160);
-	blake2s(hash, msg, NULL, RLC_MD_LEN_B2S160, len, 0);
+	blake2s(hash, RLC_MD_LEN_B2S160, msg, len, NULL, 0);
 }
 
 #endif
@@ -54,7 +54,7 @@ void md_map_b2s160(uint8_t *hash, const uint8_t *msg, int len) {
 
 void md_map_b2s256(uint8_t *hash, const uint8_t *msg, int len) {
 	memset(hash, 0, RLC_MD_LEN_B2S256);
-	blake2s(hash, msg, NULL, RLC_MD_LEN_B2S256, len, 0);
+	blake2s(hash, RLC_MD_LEN_B2S256, msg, len, NULL, 0);
 }
 
 #endif
