a0b5cf54bac7540e3bd0cf222160499e0d6665b1
Diego F. Aranha <dfaranha@gmail.com>
2021-07-13 17:47:03 +0200

    Polish membership testing in G_T for prime-order curves.

--- src/pc/relic_pc_util.c
+++ src/pc/relic_pc_util.c
@@ -267,21 +267,21 @@ int gt_is_valid(gt_t a) {
 			dv_copy(p->dp, fp_prime_get(), RLC_FP_DIGS);
 			p->used = RLC_FP_DIGS;
 			p->sign = RLC_POS;
-#ifdef GLS_MEMBER
-			/* Compute trace t = p - n + 1, and compute a^t. */
-			fp_prime_get_par(n);
-			b = fp_prime_get_par_sps(&l);
-			fp12_exp_cyc_sps((void *)v, (void *)a, b, l, RLC_POS);
-			fp12_exp_cyc_sps((void *)u, (void *)v, b, l, RLC_POS);
-			gt_sqr(v, u);
-			gt_sqr(u, v);
-			gt_mul(u, u, v);
-#else
-			/* Compute trace t = p - n + 1. */
-			bn_sub(n, p, n);
-			/* Compute u = a^t. */
-			gt_exp(u, a, n);
-#endif
+			if (ep_curve_is_pairf() == EP_BN) {
+				/* Compute trace t = p - n + 1, and compute a^t. */
+				fp_prime_get_par(n);
+				b = fp_prime_get_par_sps(&l);
+				fp12_exp_cyc_sps((void *)v, (void *)a, b, l, RLC_POS);
+				fp12_exp_cyc_sps((void *)u, (void *)v, b, l, RLC_POS);
+				gt_sqr(v, u);
+				gt_sqr(u, v);
+				gt_mul(u, u, v);
+			} else {
+				/* Compute trace t = p - n + 1. */
+				bn_sub(n, p, n);
+				/* Compute u = a^t. */
+				gt_exp(u, a, n);
+			}
 			/* Compute v = a^(p + 1). */
 			gt_frb(v, a, 1);
 			/* Check if a^(p + 1) = a^t. */
