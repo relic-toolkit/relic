31ca41446528f4c35525d5690da249ce6c0d43a1
Amine Khaldi <amine.khaldi@reactos.org>
2021-08-29 21:55:42 +0100

    Consistently pass environment CFLAGS across presets.

--- preset/armdroid-pbc-bn254.sh
+++ preset/armdroid-pbc-bn254.sh
@@ -9,5 +9,6 @@ SYSROOT=$ROOT/sysroot
 PREF=arm-linux-androideabi-
 
 export CC="$ROOT/bin/${PREF}gcc --sysroot=$SYSROOT"
+export CFLAGS="-O3 -funroll-loops -fomit-frame-pointer"
 
-cmake -DWITH="DV;BN;MD;FP;EP;FPX;EPX;PP;PC;CP" -DCHECK=off -DARITH=arm-asm-254 -DARCH=ARM -DCOLOR=off -DOPSYS=DROID -DFP_PRIME=254 -DFP_QNRES=on -DFP_METHD="INTEG;INTEG;INTEG;MONTY;EXGCD;SLIDE" -DFPX_METHD="INTEG;INTEG;LAZYR" -DPP_METHD="LAZYR;OATEP" -DCFLAGS="-O3 -funroll-loops -fomit-frame-pointer" -DLDFLAGS="-L$SYSROOT/usr/lib/gcc/arm-linux-androideabi/4.9.x/ -L$SYSROOT/usr/lib -llog" -DTIMER=HREAL -DWSIZE=32 -DSTLIB=on -DSHLIB=off $1
+cmake -DWITH="DV;BN;MD;FP;EP;FPX;EPX;PP;PC;CP" -DCHECK=off -DARITH=arm-asm-254 -DARCH=ARM -DCOLOR=off -DOPSYS=DROID -DFP_PRIME=254 -DFP_QNRES=on -DFP_METHD="INTEG;INTEG;INTEG;MONTY;EXGCD;SLIDE" -DFPX_METHD="INTEG;INTEG;LAZYR" -DPP_METHD="LAZYR;OATEP" -DLDFLAGS="-L$SYSROOT/usr/lib/gcc/arm-linux-androideabi/4.9.x/ -L$SYSROOT/usr/lib -llog" -DTIMER=HREAL -DWSIZE=32 -DSTLIB=on -DSHLIB=off $1
--- preset/x64-ecc-128.sh
+++ preset/x64-ecc-128.sh
@@ -1,2 +1,2 @@
 #!/bin/sh 
-cmake -DCHECK=off -DARITH=x64-asm-4l -DFP_PRIME=255 -DFP_QNRES=off -DEC_METHD="EDDIE" -DFP_METHD="INTEG;INTEG;INTEG;MONTY;LOWER;SLIDE" -DCFLAGS="-O3 -funroll-loops -fomit-frame-pointer -march=native -mtune=native" $1
+CFLAGS="-O3 -funroll-loops -fomit-frame-pointer -march=native -mtune=native" cmake -DCHECK=off -DARITH=x64-asm-4l -DFP_PRIME=255 -DFP_QNRES=off -DEC_METHD="EDDIE" -DFP_METHD="INTEG;INTEG;INTEG;MONTY;LOWER;SLIDE" $1
