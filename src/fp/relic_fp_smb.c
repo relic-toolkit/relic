a58b06ad872f637c259987b80ecd917cb087fb43
Diego F. Aranha <dfaranha@gmail.com>
2022-01-28 04:51:21 +0100

    Portability.

--- src/fp/relic_fp_smb.c
+++ src/fp/relic_fp_smb.c
@@ -511,11 +511,11 @@ int fp_smb_jmpds(const fp_t a) {
 
 			t0[precision] = _bn_muls_low(t0, f, RLC_SIGN(f[precision]), m[0], precision);
 			t1[precision] = _bn_muls_low(t1, g, RLC_SIGN(g[precision]), m[1], precision);
-			bn_addp_low(t0, t0, t1);
+			bn_addn_low(t0, t0, t1, precision + 1);
 
 			f[precision] = _bn_muls_low(f, f, RLC_SIGN(f[precision]), m[2], precision);
 			t1[precision] = _bn_muls_low(t1, g, RLC_SIGN(g[precision]), m[3], precision);
-			bn_addp_low(t1, t1, f);
+			bn_addn_low(t1, t1, f, precision + 1);
 
 			/* Update f and g. */
 			bn_rshs_low(f, t0, precision + 1, s);
