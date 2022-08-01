51ed41d9a1789572016a12ed9a503c1f8a187691
YuriMzz <y.mazzuoli@studenti.unipi.it>
2022-08-01 09:36:24 +0200

    ep_mul_lwreg always compiled

--- src/ep/relic_ep_mul.c
+++ src/ep/relic_ep_mul.c
@@ -670,7 +670,7 @@ void ep_mul_lwnaf(ep_t r, const ep_t p, const bn_t k) {
 
 #endif
 
-#if EP_MUL == LWREG || !defined(STRIP)
+
 
 void ep_mul_lwreg(ep_t r, const ep_t p, const bn_t k) {
 	if (bn_is_zero(k) || ep_is_infty(p)) {
@@ -683,7 +683,7 @@ void ep_mul_lwreg(ep_t r, const ep_t p, const bn_t k) {
 		ep_mul_reg_glv(r, p, k);
 		return;
 	}
-#endif
+
 
 #if defined(EP_PLAIN) || defined(EP_SUPER)
 	ep_mul_reg_imp(r, p, k);
