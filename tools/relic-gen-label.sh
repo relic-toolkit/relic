9f8e8de9ddcf38af264735ace0a1dc8099e5537c
Diego F. Aranha <dfaranha@gmail.com>
2023-04-21 12:02:05 +0200

    Fix LABEL to respect MULTI config.

--- tools/relic-gen-label.sh
+++ tools/relic-gen-label.sh
@@ -78,9 +78,15 @@ REDEF2_LOW() {
 }
 
 echo "#undef first_ctx"
-echo "#define first_ctx     RLC_PREFIX(first_ctx)"
+echo "#define first_ctx					RLC_PREFIX(first_ctx)"
 echo "#undef core_ctx"
-echo "#define core_ctx      RLC_PREFIX(core_ctx)"
+echo "#define core_ctx					RLC_PREFIX(core_ctx)"
+echo "#if defined(MULTI)"
+echo "#undef core_thread_initializer"
+echo "#define core_thread_initializer	RLC_PREFIX(core_thread_initializer)"
+echo "#undef core_init_ptr"
+echo "#define core_init_ptr				RLC_PREFIX(core_init_ptr)"
+echo "#endif"
 echo
 REDEF core
 REDEF arch
@@ -91,11 +97,11 @@ REDEF test
 REDEF util
 
 echo "#undef conf_print"
-echo "#define conf_print    RLC_PREFIX(conf_print)"
+echo "#define conf_print	RLC_PREFIX(conf_print)"
 echo
 
 echo "#undef dv_t"
-echo "#define dv_t          RLC_PREFIX(dv_t)"
+echo "#define dv_t			RLC_PREFIX(dv_t)"
 echo
 REDEF dv
 REDEF_LOW dv
@@ -110,122 +116,122 @@ REDEF_LOW bn
 
 echo "#undef fp_st"
 echo "#undef fp_t"
-echo "#define fp_st	        RLC_PREFIX(fp_st)"
-echo "#define fp_t          RLC_PREFIX(fp_t)"
+echo "#define fp_st			RLC_PREFIX(fp_st)"
+echo "#define fp_t			RLC_PREFIX(fp_t)"
 echo
 REDEF fp
 REDEF_LOW fp
 
 echo "#undef fp_st"
 echo "#undef fp_t"
-echo "#define fp_st	        RLC_PREFIX(fp_st)"
-echo "#define fp_t          RLC_PREFIX(fp_t)"
+echo "#define fp_st			RLC_PREFIX(fp_st)"
+echo "#define fp_t			RLC_PREFIX(fp_t)"
 echo
 REDEF fb
 REDEF_LOW fb
 
 echo "#undef ep_st"
 echo "#undef ep_t"
-echo "#define ep_st         RLC_PREFIX(ep_st)"
-echo "#define ep_t          RLC_PREFIX(ep_t)"
+echo "#define ep_st			RLC_PREFIX(ep_st)"
+echo "#define ep_t			RLC_PREFIX(ep_t)"
 echo
 REDEF ep
 
 echo "#undef ed_st"
 echo "#undef ed_t"
-echo "#define ed_st         RLC_PREFIX(ed_st)"
-echo "#define ed_t          RLC_PREFIX(ed_t)"
+echo "#define ed_st 		RLC_PREFIX(ed_st)"
+echo "#define ed_t			RLC_PREFIX(ed_t)"
 echo
 REDEF ed
 
 echo "#undef eb_st"
 echo "#undef eb_t"
-echo "#define eb_st         RLC_PREFIX(eb_st)"
-echo "#define eb_t          RLC_PREFIX(eb_t)"
+echo "#define eb_st			RLC_PREFIX(eb_st)"
+echo "#define eb_t			RLC_PREFIX(eb_t)"
 echo
 REDEF eb
 
 echo "#undef ep2_st"
 echo "#undef ep2_t"
-echo "#define ep2_st        RLC_PREFIX(ep2_st)"
-echo "#define ep2_t         RLC_PREFIX(ep2_t)"
+echo "#define ep2_st		RLC_PREFIX(ep2_st)"
+echo "#define ep2_t			RLC_PREFIX(ep2_t)"
 echo
 REDEF2 epx ep2
 
 echo "#undef ep3_st"
 echo "#undef ep3_t"
-echo "#define ep3_st        RLC_PREFIX(ep3_st)"
-echo "#define ep3_t         RLC_PREFIX(ep3_t)"
+echo "#define ep3_st		RLC_PREFIX(ep3_st)"
+echo "#define ep3_t			RLC_PREFIX(ep3_t)"
 echo
 REDEF2 epx ep3
 
 echo "#undef ep4_st"
 echo "#undef ep4_t"
-echo "#define ep4_st        RLC_PREFIX(ep4_st)"
-echo "#define ep4_t         RLC_PREFIX(ep4_t)"
+echo "#define ep4_st		RLC_PREFIX(ep4_st)"
+echo "#define ep4_t			RLC_PREFIX(ep4_t)"
 echo
 REDEF2 epx ep4
 
 echo "#undef fp2_st"
 echo "#undef fp2_t"
 echo "#undef dv2_t"
-echo "#define fp2_st        RLC_PREFIX(fp2_st)"
-echo "#define fp2_t         RLC_PREFIX(fp2_t)"
-echo "#define dv2_t         RLC_PREFIX(dv2_t)"
+echo "#define fp2_st		RLC_PREFIX(fp2_st)"
+echo "#define fp2_t			RLC_PREFIX(fp2_t)"
+echo "#define dv2_t			RLC_PREFIX(dv2_t)"
 echo "#undef fp3_st"
 echo "#undef fp3_t"
 echo "#undef dv3_t"
-echo "#define fp3_st        RLC_PREFIX(fp3_st)"
-echo "#define fp3_t         RLC_PREFIX(fp3_t)"
-echo "#define dv3_t         RLC_PREFIX(dv3_t)"
+echo "#define fp3_st		RLC_PREFIX(fp3_st)"
+echo "#define fp3_t			RLC_PREFIX(fp3_t)"
+echo "#define dv3_t			RLC_PREFIX(dv3_t)"
 echo "#undef fp6_st"
 echo "#undef fp6_t"
 echo "#undef dv6_t"
-echo "#define fp6_st        RLC_PREFIX(fp6_st)"
-echo "#define fp6_t         RLC_PREFIX(fp6_t)"
-echo "#define dv6_t         RLC_PREFIX(dv6_t)"
+echo "#define fp6_st		RLC_PREFIX(fp6_st)"
+echo "#define fp6_t			RLC_PREFIX(fp6_t)"
+echo "#define dv6_t			RLC_PREFIX(dv6_t)"
 echo "#undef fp9_st"
 echo "#undef fp8_t"
 echo "#undef dv8_t"
-echo "#define fp8_st        RLC_PREFIX(fp8_st)"
-echo "#define fp8_t         RLC_PREFIX(fp8_t)"
-echo "#define dv8_t         RLC_PREFIX(dv8_t)"
+echo "#define fp8_st		RLC_PREFIX(fp8_st)"
+echo "#define fp8_t			RLC_PREFIX(fp8_t)"
+echo "#define dv8_t			RLC_PREFIX(dv8_t)"
 echo "#undef fp9_st"
 echo "#undef fp9_t"
 echo "#undef dv9_t"
-echo "#define fp9_st        RLC_PREFIX(fp9_st)"
-echo "#define fp9_t         RLC_PREFIX(fp9_t)"
-echo "#define dv9_t         RLC_PREFIX(dv9_t)"
+echo "#define fp9_st		RLC_PREFIX(fp9_st)"
+echo "#define fp9_t			RLC_PREFIX(fp9_t)"
+echo "#define dv9_t			RLC_PREFIX(dv9_t)"
 echo "#undef fp12_st"
 echo "#undef fp12_t"
 echo "#undef dv12_t"
-echo "#define fp12_st        RLC_PREFIX(fp12_st)"
-echo "#define fp12_t         RLC_PREFIX(fp12_t)"
-echo "#define dv12_t         RLC_PREFIX(dv12_t)"
+echo "#define fp12_st		RLC_PREFIX(fp12_st)"
+echo "#define fp12_t		RLC_PREFIX(fp12_t)"
+echo "#define dv12_t		RLC_PREFIX(dv12_t)"
 echo "#undef fp18_st"
 echo "#undef fp18_t"
 echo "#undef dv18_t"
-echo "#define fp18_st        RLC_PREFIX(fp18_st)"
-echo "#define fp18_t         RLC_PREFIX(fp18_t)"
-echo "#define dv18_t         RLC_PREFIX(dv18_t)"
+echo "#define fp18_st		RLC_PREFIX(fp18_st)"
+echo "#define fp18_t		RLC_PREFIX(fp18_t)"
+echo "#define dv18_t		RLC_PREFIX(dv18_t)"
 echo "#undef fp24_st"
 echo "#undef fp24_t"
 echo "#undef dv24_t"
-echo "#define fp24_st        RLC_PREFIX(fp24_st)"
-echo "#define fp24_t         RLC_PREFIX(fp24_t)"
-echo "#define dv24_t         RLC_PREFIX(dv24_t)"
+echo "#define fp24_st		RLC_PREFIX(fp24_st)"
+echo "#define fp24_t		RLC_PREFIX(fp24_t)"
+echo "#define dv24_t		RLC_PREFIX(dv24_t)"
 echo "#undef fp48_st"
 echo "#undef fp48_t"
 echo "#undef dv48_t"
-echo "#define fp48_st        RLC_PREFIX(fp48_st)"
-echo "#define fp48_t         RLC_PREFIX(fp48_t)"
-echo "#define dv48_t         RLC_PREFIX(dv48_t)"
+echo "#define fp48_st		RLC_PREFIX(fp48_st)"
+echo "#define fp48_t		RLC_PREFIX(fp48_t)"
+echo "#define dv48_t		RLC_PREFIX(dv48_t)"
 echo "#undef fp54_st"
 echo "#undef fp54_t"
 echo "#undef dv54_t"
-echo "#define fp54_st        RLC_PREFIX(fp54_st)"
-echo "#define fp54_t         RLC_PREFIX(fp54_t)"
-echo "#define dv54_t         RLC_PREFIX(dv54_t)"
+echo "#define fp54_st		RLC_PREFIX(fp54_st)"
+echo "#define fp54_t		RLC_PREFIX(fp54_t)"
+echo "#define dv54_t		RLC_PREFIX(dv54_t)"
 echo
 REDEF2 fpx fp2
 REDEF2_LOW fpx fp2
@@ -254,8 +260,8 @@ echo "#undef rabin_t"
 echo "#undef phpe_t"
 echo "#undef bdpe_t"
 echo "#undef sokaka_t"
-echo "#define crt_t	RLC_PREFIX(crt_t)"
-echo "#define rsa_t	RLC_PREFIX(rsa_t)"
+echo "#define crt_t		RLC_PREFIX(crt_t)"
+echo "#define rsa_t		RLC_PREFIX(rsa_t)"
 echo "#define rabin_t	RLC_PREFIX(rabin_t)"
 echo "#define phpe_t	RLC_PREFIX(phpe_t)"
 echo "#define bdpe_t	RLC_PREFIX(bdpe_t)"
