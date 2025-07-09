26eb8f921932b5bd88132f105373e415b871d4b6
Diego F. Aranha <dfaranha@gmail.com>
2025-07-09 02:18:26 +0200

    We keep trying.

--- src/low/gmp-sec/relic_fp_inv_low.c
+++ src/low/gmp-sec/relic_fp_inv_low.c
@@ -54,7 +54,8 @@ void fp_invm_low(dig_t *c, const dig_t *a) {
 	fp_copy(u, a);
 #endif
 
-	mpn_sec_invert(c, u, fp_prime_get(), RLC_FP_DIGS, cnt, t);
+	mpn_sec_invert((mp_ptr)c, (mp_srcptr)u, (mp_srcptr)fp_prime_get(),
+		RLC_FP_DIGS, cnt, (mp_ptr)t);
 
 #if FP_RDC == MONTY
 	dv_zero(s, RLC_FP_DIGS);
